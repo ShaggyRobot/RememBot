c01748254ed7783af5995ba469072634e86c2585
WTFi4 <ShaggyRobot@gmail.com>
2021-02-13 00:33:09 +0300

    Renaming functions & variables + Rearranging code chunks + Trying to implement @retries decorator

--- TGBot_tele.py
+++ TGBot_tele.py
@@ -1,13 +1,13 @@
 import config
 import Scheduler
 from date_parse import date_parse
-from datetime import datetime
 
 import telebot
 from telebot import types
-from telebot.types import ReplyKeyboardRemove as keyboard_remove
+from telebot.types import ReplyKeyboardRemove as ReplyKeyboardRemove
 
-bot = telebot.TeleBot(config.token, parse_mode=None)
+bot = telebot.TeleBot(config.token)
+handler_STATE = 'init'
 
 markup = types.ReplyKeyboardMarkup(row_width=4, resize_keyboard=True)
 buttons = (types.KeyboardButton('10m'),
@@ -26,12 +26,34 @@ def send_msg(chat_id: int, text: str, job_name: str):
     :param job_name: job name
     :return: none
     """
-    print('<<<', config.time_now(), job_name)
-    bot.send_message(chat_id=chat_id, text=text)
+    for _ in range(5):
+        try:
+            print('<<<', config.time_now(), job_name)
+            bot.send_message(chat_id=chat_id, text=text)
+            break
+        except Exception:
+            print('!!!Exception in send_msg!!!')
+            continue
+
+
+def retries(func):
+    """Retry decorator"""
+    # todo @retry decorator (import wraps maybe?)
+    print('Retry')
+
+    def wrapper(*args, **kwargs):
+        for i in range(5):
+            try:
+                func(*args, **kwargs)
+                break
+            except Exception:
+                print(f'Exception occured, {i}...')
+                continue
+
+    return wrapper()
 
 
 if __name__ == '__main__':
-    trigger = 'init'
     txt = ''
 
 
@@ -40,34 +62,42 @@ if __name__ == '__main__':
         Scheduler.print_jobs()
 
 
-    @bot.message_handler(func=lambda x: trigger == 'init')
+    @bot.message_handler(func=lambda x: handler_STATE == 'init')
+    # todo Make this decorator for retries work here!!!
+    # @ retry
     def message_listen(update):
-        global trigger
+        global handler_STATE
         global txt
         txt = update.text
         bot.send_message(chat_id=update.chat.id,
                          text='When to remind\n{}?'.format(txt),
                          reply_markup=markup)
-        trigger = 'date_listen'
+        handler_STATE = 'date_listen'
 
 
-    @bot.message_handler(func=lambda x: trigger == 'date_listen')
+    @bot.message_handler(func=lambda x: handler_STATE == 'date_listen')
     def date_listen(update):
         global txt
-        global trigger
-        if date_parse(update.text) == 'FUCK!!':
-            bot.send_message(update.chat.id, 'FUCK!!!', reply_markup=keyboard_remove())
-        else:
-            time = date_parse(update.text)
-            msg = '"{}"\n is set at\n {}'.format(txt, time.strftime('%H:%M:%S %d.%m.%Y'))
-            Scheduler.add_job(time=time,
-                              text=txt,
-                              chat_id=update.chat.id,
-                              job_name='{} * {}'.format(update.chat.username,
-                                                        update.text))
-            bot.send_message(update.chat.id, msg, reply_markup=keyboard_remove())
+        global handler_STATE
+        for _ in range(5):
+            try:
+                if date_parse(update.text) == 'FUCK!!':
+                    bot.send_message(update.chat.id, 'FUCK!!!', reply_markup=ReplyKeyboardRemove())
+                else:
+                    time = date_parse(update.text)
+                    msg = '"{}"\n is set at\n {}'.format(txt, time.strftime('%H:%M:%S %d.%m.%Y'))
+                    Scheduler.add_job(time=time,
+                                      text=txt,
+                                      chat_id=update.chat.id,
+                                      job_name='{} * {}'.format(update.chat.username,
+                                                                update.text))
+                    bot.send_message(update.chat.id, msg, reply_markup=ReplyKeyboardRemove())
+                break
+            except Exception:
+                print('!!!Exception in date_listen!!!')
+                continue
         txt = ''
-        trigger = 'init'
+        handler_STATE = 'init'
 
 
     print('beep')
@@ -75,4 +105,10 @@ if __name__ == '__main__':
     Scheduler.scheduler.start()
     Scheduler.print_jobs()
 
-    bot.infinity_polling(True)
+    for _ in range(5):
+        try:
+            bot.infinity_polling(True)
+            # break
+        except Exception:
+            print('Exception in bot.infinity_polling')
+            continue
